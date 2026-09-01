5d955c32b6d29aa68dfaae9f94f7236ee429a571
ChryzzMT <chryztianmt@gmail.com>
2026-09-01 10:18:04 -0400

    Update SumaArrEnRAM.c

--- Practicas/SumaArrEnRAM.c
+++ Practicas/SumaArrEnRAM.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+int main(){
+  int arreglo[]={12,34,56};
+  int i=2,suma=0;
+  while(i>=0){
+    suma+=arreglo[i];
+    i=i-1;
+  }
+  printf("suma:%d\n",suma);
+  return 0;
+}
